df29bf2a57f112c2bd65e7b36f40f352465989a7
KaemonIsland <k-manskrazy@hotmail.com>
2019-06-10 19:53:32 -0600

    Extracted model into its own component

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import ProductImage from './ProductImage'
+import ProductModel from './ProductModel';
 
 const ProductContainer = styled.div`
     border: 1px solid black;
@@ -14,56 +15,6 @@ const ProductContainer = styled.div`
     box-shadow: 0 .3rem .3rem rgba(0, 0, 0, .5);
 `
 
-const Input = styled.input`
-    display: none;
-`
-
-const Overlay = styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    z-index: 99;
-    background: #000;
-    pointer-events: none;
-    opacity: 0;
-    transform: scale(0.5);
-
-    ${Input}:checked ~ & {
-        pointer-events: auto;
-        opacity: 0.8;
-        transform: scale(1);
-        transition: transform 0.5s ease, opacity 0.5s ease;
-    }
-`
-
-const Modal = styled.div`
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    z-index: 100;
-    transform: translate(-50%, -50%) scale(0.5);
-    width: 100%;
-    max-width: 640px;
-    pointer-events: none;
-    opacity: 0;
-
-    ${Input}:checked ~ & {
-        pointer-events: auto;
-        opacity: 1;
-        transform: translate(-50%, -50%) scale(1);
-        transition: transform 0.5s ease, opacity 0.5s ease;
-        transition-delay: 0.1s;
-    }
-`
-
-const ModalContent = styled.div`
-    display: block;
-    padding: 3rem;
-    background: white;
-`
-
 
 
 const Product = ({name, id, description, img, price, rating, seller}) => {
@@ -71,22 +22,14 @@ const Product = ({name, id, description, img, price, rating, seller}) => {
         <ProductContainer>
             <ProductImage img={img} />
             <p>Rating: {rating} Stars</p>
-            <Input type="checkbox" id={id} />
-            <label className="button" htmlFor={id} >
-                <div><strong>{name}</strong> ${price}</div>
-            </label>
-            <Modal>
-                <ModalContent>
-                    <ProductImage img={img} />
-                    <h3><strong>{name}</strong> - ${price}</h3>
-                    <p>{description}</p>
-                    <p>Sold by: <em>{seller}</em></p>
-                    <label htmlFor={id} className="button" >
-                        <a>Close</a>
-                    </label>
-                </ModalContent>
-            </Modal>
-            <Overlay></Overlay>
+            <ProductModel 
+                id={id}
+                name={name}
+                description={description}
+                img={img}
+                price={price}
+                seller={seller}
+            />
         </ProductContainer>
     )
 }
---
+++ src/components/products/ProductModel.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import styled from 'styled-components'
+import ProductImage from './ProductImage'
+
+const Input = styled.input`
+    display: none;
+`
+
+const Overlay = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: 99;
+    background: #000;
+    pointer-events: none;
+    opacity: 0;
+    transform: scale(0.5);
+
+    ${Input}:checked ~ & {
+        pointer-events: auto;
+        opacity: 0.8;
+        transform: scale(1);
+        transition: transform 0.5s ease, opacity 0.5s ease;
+    }
+`
+
+const Modal = styled.div`
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    z-index: 100;
+    transform: translate(-50%, -50%) scale(0.5);
+    width: 100%;
+    max-width: 640px;
+    pointer-events: none;
+    opacity: 0;
+
+    ${Input}:checked ~ & {
+        pointer-events: auto;
+        opacity: 1;
+        transform: translate(-50%, -50%) scale(1);
+        transition: transform 0.5s ease, opacity 0.5s ease;
+        transition-delay: 0.1s;
+    }
+`
+
+const ModalContent = styled.div`
+    display: block;
+    padding: 3rem;
+    background: white;
+`
+
+const Label = styled.label`
+    cursor: pointer;
+    border-bottom: 2px solid transparent;
+    transition: all .3s ease-out;
+    &:hover {
+        border-bottom: 2px solid black;
+    }
+`
+
+const ProductModel = ({id, img, name, price, description, seller}) => {
+    return (
+    <>
+        <Input type="checkbox" id={id} />
+            <Label htmlFor={id} >
+                <div><strong>{name}</strong> ${price}</div>
+            </Label>
+            <Modal>
+                <ModalContent>
+                    <ProductImage img={img} />
+                    <h3><strong>{name}</strong> - ${price}</h3>
+                    <p>{description}</p>
+                    <p>Sold by: <em>{seller}</em></p>
+                    <Label htmlFor={id}>
+                        <a><strong>Close</strong></a>
+                    </Label>
+                </ModalContent>
+            </Modal>
+        <Overlay></Overlay>
+    </>
+    )
+}
+
+export default ProductModel;
